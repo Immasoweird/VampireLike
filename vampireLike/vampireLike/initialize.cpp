42f24ef857c7558c22adc04948cefe5b56b9a654
SonSatanS <danilshybin02@gmail.com>
2025-05-14 19:44:01 +0300

    Исправление бага с появлением текстур на врагах
    Теперь текстуры врагов появляются заново после перезапуска

--- vampireLike/vampireLike/initialize.cpp
+++ vampireLike/vampireLike/initialize.cpp
@@ -75,7 +75,7 @@ void InitEnemies(int enemiesNumber) {
 				body,
 				speed,
 				position,
-			{0,0},
+				{0,0},
 				color,
 				active,
 				false,
@@ -83,6 +83,8 @@ void InitEnemies(int enemiesNumber) {
 				damage,
 
 		};
+		current.enemyWalkTextureRight = enemyTextureRight;
+		current.enemyWalkTextureLeft = enemyTextureLeft;
 		enemies.push_back(current);
 	}
 }
@@ -144,12 +146,12 @@ void InitGame() {
 			damage,
 		};
 	}
-
 	InitEnemies(10);
 	if (!spritesLoaded) {
 		LoadTextures();
 		spritesLoaded = true;
 	}
+
 }
 
 void LoadTextures() {
@@ -182,18 +184,18 @@ void LoadTextures() {
 
 	Image image3 = LoadImage("assets\\SkeletonWalkRight.png");
 	ImageResize(&image3, image3.width * 3, image3.height * 3);
-	enemyTexture = LoadTextureFromImage(image3);
+	enemyTextureRight = LoadTextureFromImage(image3);
 	for (int i = 0; i < MAX_ENEMIES; i++) {
-		enemies[i].enemyWalkTextureRight = enemyTexture;
+		enemies[i].enemyWalkTextureRight = enemyTextureRight;
 	}
 	UnloadImage(image3);
 
 	image3 = LoadImage("assets\\SkeletonWalkLeft.png");
 	ImageResize(&image3, image3.width * 3, image3.height * 3);
 	ImageFlipHorizontal(&image3);
-	enemyTexture = LoadTextureFromImage(image3);
+	enemyTextureLeft = LoadTextureFromImage(image3);
 	for (int i = 0; i < MAX_ENEMIES; i++) {
-		enemies[i].enemyWalkTextureLeft = enemyTexture;
+		enemies[i].enemyWalkTextureLeft = enemyTextureLeft;
 	}
 	UnloadImage(image3);
 
